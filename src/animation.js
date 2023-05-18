7407ad7fb564a24023c4ad1d8b7003352627e16f
Steven Chan <richlife1220@gmail.com>
2023-05-19 00:05:46 +0800

    修改動畫並加入 rwd

--- src/animation.js
+++ src/animation.js
@@ -7,12 +7,14 @@ AOS.init();
 
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
+// 自動打字效果
 gsap.to('.typing', {
   text: '網頁設計',
   duration: 3,
   repeat: -1,
 });
 
+// 跑馬燈
 gsap.to('.loop', {
   x: -1000,
   ease: 'none',
@@ -47,6 +49,7 @@ const topicMainCard = document.querySelector('.main-card-content');
 //   });
 // }
 
+// 三大主題區塊由右向左分段移入
 gsap.set(topicMainCard, { xPercent: 85 });
 let time = gsap.timeline({
   scrollTrigger: {
@@ -58,16 +61,38 @@ let time = gsap.timeline({
   },
 });
 
-time
-  .to(topicMainCard, {
-    xPercent: 40,
-    duration: 50,
-    ease: 'expo',
-    overwrite: 'auto',
-  })
-  .to(topicMainCard, {
-    xPercent: 0,
+let mm = gsap.matchMedia();
+
+mm.add('(min-width: 768px)', () => {
+  time
+    .to(topicMainCard, {
+      xPercent: 40,
+      duration: 50,
+      ease: 'expo',
+      overwrite: 'auto',
+    })
+    .to(topicMainCard, {
+      xPercent: 0,
+      duration: 50,
+      ease: 'expo',
+      overwrite: 'auto',
+    });
+});
+
+mm.add('(max-width: 767px)', () => {
+  time.to(topicMainCard, {
+    xPercent: 24,
     duration: 50,
     ease: 'expo',
     overwrite: 'auto',
   });
+});
+
+mm.add('(max-width: 375px)', () => {
+  time.to(topicMainCard, {
+    xPercent: 15,
+    duration: 50,
+    ease: 'expo',
+    overwrite: 'auto',
+  });
+});
